16f1a5997c4961e092eb59303f2f48e00ef75332
cage <cbull358@gmail.com>
2026-09-09 13:45:48 -0400

    GET endpoint greated

--- index.js
+++ index.js
@@ -10,19 +10,56 @@ const conf = {
 };
 
 const app = express();
+app.use(express.json());
 const pool = new Pool(conf);
-const client = await pool.connect();
+// const client = await pool.connect();
 
+
+app.get('/api/questions', async (req, res) => {
+	const { index, topic, difficulty } = req.query;
+	let params = []
+	const client = await pool.connect();
+	try{	
+		let queryText = 'GET * FROM questions WHERE 1=1';
+		if (index) {
+			params.push(parseInt(index));
+			queryText += ` AND index=$${params.length}`;
+		}
+
+		if(topic) {
+			params.push(topic);
+			queryText += ` AND topic=$${params.length}`;
+		}
+
+		if(difficulty) {
+			params.push(difficulty);
+			queryText += ` AND difficulty=$${params.length}`;
+		}
+
+		const rows = await client.query(queryText, params);
+		if(res.rows.length === 0){
+			res.status(404).json({ error: "No such question matches those parameters" });
+		} else {
+			res.json(result.rows);
+		}
+	} catch (e) {
+		await client.query('ROLLBACK');
+		res.status(500);
+	} finally {
+		client.release();
+	}
+});
 
 
 app.listen(parseInt(process.env.API_PORT), async () => {
-	await pool.query(`CREATE TABLE IF NOT EXISTS questions(
+	let res = await pool.query(`CREATE TABLE IF NOT EXISTS questions(
 		index INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 		content TEXT NOT NULL,
 		answer TEXT NOT NULL,
 		topic TEXT NOT NULL,
 		difficulty INT NOT NULL
 	)`);
-	console.log(`UNC trivia api running on port ${PORT}`);
+	console.log(`Successfully created table questions with result: ${res}`);
+	console.log(`UNC trivia api running on port ${process.env.API_PORT}`);
 });
-await client.release();
+// await client.release();
